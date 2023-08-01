046b1e040b3bdaa22037108576beaf89f49069af
triciadrich <triciadrich@gmail.com>
2023-08-01 14:59:53 -0700

    skeleton

---
+++ src/test/java/com/example/slopemates/SlopeMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slopemates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlopeMatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
